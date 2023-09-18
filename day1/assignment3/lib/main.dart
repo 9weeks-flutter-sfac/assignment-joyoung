fee6efad7590ffa49ff0c8ec524409adeaeeea1c
Jo Young <githuborigin@gmail.com>
2023-09-18 17:40:01 +0900

    시구절 과제 구현

--- day1/assignment3/lib/main.dart
+++ day1/assignment3/lib/main.dart
@@ -10,30 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     String st1 =
-        '이듬해 질 녘 꽃 피는 봄 한여름 밤의 꿈 \n가을 타 겨울 내릴 눈 일 년 네 번 또다시 봄\n정들었던 내 젊은 날 이제는 안녕\n아름답던 우리의 봄 여름 가을 겨울\n(Four seasons with no reason)\n\n';
+        '사랑을 다해 사랑을 하였노라고 \n정작 해야 할 말이 남아 있었음을 알았을 때 \n당신은 이미 남의 사람이 되어 있었다\n\n';
     String st2 =
-        '비 갠 뒤에 비애 대신 a happy end\n비스듬히 씩 비웃듯 칠색 무늬의 무지개\n철없이 철 지나 철들지 못해 (still)\n철부지에 철 그른지 오래\n\n';
-    String st3 = 'Marchin\' 비발디, 차이코프스키\n오늘의 사계를 맞이해 (boy)\n마침내 마치 넷이 못내 \n\n';
+        '불러야 할 뜨거운 노래를 가슴으로 죽이고 \n당신은 멀리 이루어지고 있었다\n\n하마 곱스런 눈 웃음이 사라지기 전\n두고두고 아름다움으로 잊어 달라지만 \n남자에게서 여자란 기쁨 아니면 슬픔\n\n다섯 손가락 끝을 잘라 핏물 오선을 그려\n혼자라도 외롭지 않을 밤에 울어보리라 \n\n';
+    String st3 = '울어서 멍든 눈흘김으로\n미워서 미워지도록 사랑하리라 \n\n';
     String st4 =
-        '저 하늘만 바라보고서\n사계절 잘 지내고 있어, goodbye\n떠난 사람 또 나타난 사람\n머리 위 저세상, 난 떠나 영감의 amazon\n\n';
-    String st5 =
-        '지난 밤의 트라우마 다 묻고\n목숨 바쳐 달려올 새 출발 하는 왕복선\n변할래 전보다는 더욱더\n좋은 사람 더욱더, 더 나은 사람 더욱더\n\n';
-    String st6 =
-        '아침 이슬을 맞고 (내 안에)\n내 안에 분노 과거에 묻고\nFor life, do it away, away, away \n\n';
+        '한 잔은 떠나버린 너를 위하여 \n또 한 잔은 너와의 영원한 사랑을 위하여 \n그리고 또 한 잔은 이미 초라해진 나를 위하여 \n마지막 한 잔은 미리 알고 정하신 하나님을 위하여';
 
     return MaterialApp(
       home: Scaffold(
         body: Center(
           child: RichText(
-            textAlign: TextAlign.center,
             text: TextSpan(
               children: [
-                TextSpan(text: st1, style: TextStyle(color: Colors.lightBlue)),
-                TextSpan(text: st2, style: TextStyle(color: Colors.red)),
-                TextSpan(text: st3, style: TextStyle(color: Colors.green)),
-                TextSpan(text: st4, style: TextStyle(color: Colors.grey)),
-                TextSpan(text: st5, style: TextStyle(color: Colors.yellow)),
-                TextSpan(text: st6, style: TextStyle(color: Colors.blue)),
+                TextSpan(
+                    text: st1,
+                    style: TextStyle(
+                        color: Colors.grey[400], fontStyle: FontStyle.italic)),
+                TextSpan(
+                    text: st2,
+                    style: TextStyle(
+                        color: Colors.grey[500], fontStyle: FontStyle.italic)),
+                TextSpan(
+                    text: st3,
+                    style: TextStyle(
+                        color: Colors.grey[700], fontStyle: FontStyle.italic)),
+                TextSpan(
+                    text: st4,
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontStyle: FontStyle.italic)),
               ],
             ),
           ),
