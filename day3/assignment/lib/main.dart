27f10ae0cf261453394e8fd20d5f36361f55e0ff
Jo Young <githuborigin@gmail.com>
2023-09-20 11:38:11 +0900

    BttomNavigation widget 강의 코드

---
+++ day3/assignment/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('플러터좋아요'),
+        ),
+        body: Text('asdfa'),
+        bottomNavigationBar: BottomNavigationBar(items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: '홈'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: '검색'),
+        ]),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          backgroundColor: Colors.black12,
+          elevation: 0,
+
+          child: Icon(Icons.add),
+          mini: true,
+        ),
+      ),
+    );
+  }
+}
