c9eb0a7514fee997e11d5ae456ff301aeac84cd1
Jo Young <githuborigin@gmail.com>
2023-10-05 18:38:24 +0900

    set state 실행해서 상태 변경을 해줄때 변수의 선언을 빌드 이전으로 해줬어야했음 이것 때문에 이미지가 안떠서 고생을 했습니다

--- day10/assignment/lib/main.dart
+++ day10/assignment/lib/main.dart
@@ -1,19 +1,65 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-
-
 //image_picker: ^1.0.4
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var ImagePickers = ImagePicker();
+  XFile? seletedImage;
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp( home: Scaffold(body: Column(children: [],)),);
+    if (seletedImage != null) {
+      print(seletedImage?.path);
+    } else {
+      print(seletedImage?.path);
+    }
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (seletedImage != null) Image.file(File(seletedImage!.path)),
+            if (seletedImage == null) Text('이미지 없음'),
+            TextButton(
+                onPressed: () async {
+                  var image =
+                      await ImagePickers.pickImage(source: ImageSource.gallery);
+                  if (image != null) {
+                    print('이미지가 선택 되었습니다');
+                    seletedImage = image;
+                    print("이미지 경로 ${seletedImage?.path}");
+
+                    setState(() {
+                      seletedImage = image;
+                    });
+                  } else {
+                    print('선택되지 않습니다');
+                  }
+                },
+                child: Text('select Image'))
+          ],
+        ),
+      )),
+    );
   }
 }
-  // This wist
+
+var ImagePickers = ImagePicker();
+XFile? seletedImage;
+// 갤러리에서 이미지 선택
+
