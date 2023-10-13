55b269e21112bfdf8c13acb0db62952351cfbc97
Jo Young <githuborigin@gmail.com>
2023-10-14 02:13:13 +0900

    82번 과제 완료

---
+++ day13/assignment4/lib/model/model.dart
@@ -0,0 +1,39 @@
+import 'dart:convert';
+
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+class Model {
+  int userId;
+  int id;
+  String title;
+  String body;
+  Model({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'userId': userId,
+      'id': id,
+      'title': title,
+      'body': body,
+    };
+  }
+
+  factory Model.fromMap(Map<String, dynamic> map) {
+    return Model(
+      userId: map['userId'] as int,
+      id: map['id'] as int,
+      title: map['title'] as String,
+      body: map['body'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Model.fromJson(String source) =>
+      Model.fromMap(json.decode(source) as Map<String, dynamic>);
+}
--- day13/assignment4/lib/utils/apiutil.dart
+++ day13/assignment4/lib/utils/apiutil.dart
@@ -1,15 +1,15 @@
-import 'package:assignment3/model/dogmodel.dart';
+import 'package:assignment/model/model.dart';
 import 'package:dio/dio.dart';
 
-Future<DogModel> fetchData(String url) async {
+Future<List<Model>> fetchData(String url) async {
   final dio = Dio();
 
   try {
     Response response = await dio.get(url);
 
     if (response.statusCode == 200) {
-      DogModel Dog = DogModel.fromMap(response.data);
-      print(Dog.toMap());
+      List<Model> Dog =
+          List<Model>.from(response.data.map((e) => Model.fromMap(e)));
       return Dog;
     } else {
       throw Exception('Failed to load data');
